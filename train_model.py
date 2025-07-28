1b718681f72bf7f6bb4115b61bad3b1875be4287
Jim Dewantara <54344826+jimbotake@users.noreply.github.com>
2025-07-28 14:50:58 +0700

    train_model.py

--- train_model.py
+++ train_model.py
@@ -8,7 +8,7 @@ conn = psycopg2.connect(
     host="localhost",
     database="prediksi_db",
     user="postgres",
-    password="Humaira31814"
+    password="yourpassword"
 )
 
 # Ambil data dari DB
